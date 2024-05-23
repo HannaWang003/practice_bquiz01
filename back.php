5d2cc8e34b1f180d7d2a5cfddb3998d0b553b3e8
HannaWang003 <hannawang003@gmail.com>
2024-05-23 10:19:16 +0800

    整理後台畫面，完成登出API

--- back.php
+++ back.php
@@ -22,7 +22,6 @@ include_once "./api/db.php";
             <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
         </div>
     </div>
-    <iframe style="display:none;" name="back" id="back"></iframe>
     <div id="main">
         <a title="" href="?">
             <div class="ti" style="background:url('use/'); background-size:cover;"></div>
@@ -80,6 +79,17 @@ include_once "./api/db.php";
             <div class="di"
                 style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
                 <!--正中央-->
+                <table width="100%">
+                    <tbody>
+                        <tr>
+                            <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
+                                class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
+                            </td>
+                            <td><button onclick="location.href='./api/logout.php'"
+                                    style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
+                        </tr>
+                    </tbody>
+                </table>
                 <?php
 				$do = $_GET['do'] ?? "title";
 				$file = "./back/{$do}.php";
@@ -91,30 +101,12 @@ include_once "./api/db.php";
 
 				?>
             </div>
-            <div id="alt"
-                style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
-            </div>
-            <script>
-            $(".sswww").hover(
-                function() {
-                    $("#alt").html("" + $(this).children(".all").html() + "").css({
-                        "top": $(this).offset().top - 50
-                    })
-                    $("#alt").show()
-                }
-            )
-            $(".sswww").mouseout(
-                function() {
-                    $("#alt").hide()
-                }
-            )
-            </script>
         </div>
         <div style="clear:both;"></div>
         <div
             style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
             <span class="t" style="line-height:123px;">
-                <?=$Bottom->find(1)['bottom']?>
+                <?= $Bottom->find(1)['bottom'] ?>
             </span>
         </div>
     </div>
--- back/title.php
+++ back/title.php
@@ -1,13 +1,3 @@
-<table width="100%">
-    <tbody>
-        <tr>
-            <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;" class="cent"><a
-                    href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a></td>
-            <td><button onclick="document.cookie='user=';location.replace('?')"
-                    style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
-        </tr>
-    </tbody>
-</table>
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">網站標題管理</p>
     <form method="post" target="back" action="?do=tii">
@@ -25,8 +15,7 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')"
-                            value="新增網站標題圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
