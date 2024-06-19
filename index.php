c635a12f173210453dcf48d96dd50c9fa8174199
HannaWang003 <hannawang003@gmail.com>
2024-06-19 14:56:27 +0800

    done:menu

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 include_once "./api/db.php";
 ?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -14,6 +13,16 @@ include_once "./api/db.php";
     <script src="./js/jquery-3.4.1.js"></script>
     <script src="./js/js.js"></script>
 </head>
+<style>
+    .submenu {
+        position: absolute;
+        top: 50%;
+        left: 80%;
+        display: none;
+        z-index: 100;
+
+    }
+</style>
 
 <body>
     <div id="cover" style="display:none; ">
@@ -25,8 +34,7 @@ include_once "./api/db.php";
     <iframe style="display:none;" name="back" id="back"></iframe>
     <div id="main">
         <a title="" href="./index.php">
-            <div class="ti"
-                style="background:url('../img/<?= $Title->find(['sh' => 1])['img'] ?>'); background-size:cover;"></div>
+            <div class="ti" style="background:url('../img/<?= $Title->find(['sh' => 1])['img'] ?>'); background-size:cover;"></div>
             <!--標題-->
         </a>
         <div id="ms">
@@ -34,14 +42,37 @@ include_once "./api/db.php";
                 <div id="menuput" class="dbor">
                     <!--主選單放此-->
                     <span class="t botli">主選單區</span>
+                    <?php
+                    $menus = $Menu->all(['sh' => 1, 'big_id' => 0]);
+                    foreach ($menus as $menu) {
+                        $submenus = $Menu->all(['sh' => 1, 'big_id' => $menu['id']]);
+                    ?>
+                        <div style="position:relative;" class="menuitem">
+                            <div class="topmenu"><?= $menu['text'] ?></div>
+                            <div class="submenu">
+                                <?php
+                                foreach ($submenus as $submenu) {
+                                ?>
+                                    <div class="mainmu2">
+                                        <?= $submenu['text']; ?>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                            </div>
+                        </div>
+
+                    <?php
+                    }
+
+                    ?>
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
                         <?= $Total->find(1)['total'] ?></span>
                 </div>
             </div>
-            <div class="di"
-                style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+            <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
                 <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
                 </marquee>
                 <div style="height:32px; display:block;"></div>
@@ -57,56 +88,60 @@ include_once "./api/db.php";
 
                 ?>
             </div>
-            <div id="alt"
-                style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+            <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
             </div>
             <script>
-            $(".sswww").hover(
-                function() {
-                    $("#alt").html("" + $(this).children(".all").html() + "").css({
-                        "top": $(this).offset().top - 50
-                    })
-                    $("#alt").show()
-                }
-            )
-            $(".sswww").mouseout(
-                function() {
-                    $("#alt").hide()
-                }
-            )
+                $(".sswww").hover(
+                    function() {
+                        $("#alt").html("" + $(this).children(".all").html() + "").css({
+                            "top": $(this).offset().top - 50
+                        })
+                        $("#alt").show()
+                    }
+                )
+                $(".sswww").mouseout(
+                    function() {
+                        $("#alt").hide()
+                    }
+                )
+                $('.menuitem').hover(function() {
+                    $('.submenu').hide();
+                    $(this).children('.submenu').show();
+                }, function() {
+                    $('.submenu').hide()
+
+                })
             </script>
             <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
                 <!--右邊-->
-                <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
-                    onclick="lo('?do=login')">管理登入</button>
+                <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
                     <script>
-                    var nowpage = 0,
-                        num = 0;
+                        var nowpage = 0,
+                            num = 0;
 
-                    function pp(x) {
-                        var s, t;
-                        if (x == 1 && nowpage - 1 >= 0) {
-                            nowpage--;
+                        function pp(x) {
+                            var s, t;
+                            if (x == 1 && nowpage - 1 >= 0) {
+                                nowpage--;
+                            }
+                            if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+                                nowpage++;
+                            }
+                            $(".im").hide()
+                            for (s = 0; s <= 2; s++) {
+                                t = s * 1 + nowpage * 1;
+                                $("#ssaa" + t).show()
+                            }
                         }
-                        if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
-                            nowpage++;
-                        }
-                        $(".im").hide()
-                        for (s = 0; s <= 2; s++) {
-                            t = s * 1 + nowpage * 1;
-                            $("#ssaa" + t).show()
-                        }
-                    }
-                    pp(1)
+                        pp(1)
                     </script>
                 </div>
             </div>
         </div>
         <div style="clear:both;"></div>
-        <div
-            style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+        <div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
             <span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom'] ?></span>
         </div>
     </div>
