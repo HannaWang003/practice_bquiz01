8bbaa861ffab7e9689172f2cf275b1bcba74f62c
HannaWang003 <hannawang003@gmail.com>
2024-06-19 15:35:17 +0800

    done:images

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include_once "./api/db.php";
     .submenu {
         position: absolute;
         top: 50%;
-        left: 80%;
+        left: 60%;
         display: none;
         z-index: 100;
 
@@ -48,14 +48,18 @@ include_once "./api/db.php";
                         $submenus = $Menu->all(['sh' => 1, 'big_id' => $menu['id']]);
                     ?>
                         <div style="position:relative;" class="menuitem">
-                            <div class="topmenu"><?= $menu['text'] ?></div>
+                            <a href="<?= $menu['url'] ?>">
+                                <div class="topmenu"><?= $menu['text'] ?></div>
+                            </a>
                             <div class="submenu">
                                 <?php
                                 foreach ($submenus as $submenu) {
                                 ?>
-                                    <div class="mainmu2">
-                                        <?= $submenu['text']; ?>
-                                    </div>
+                                    <a href="<?= $submenu['url'] ?>">
+                                        <div class="mainmu2">
+                                            <?= $submenu['text']; ?>
+                                        </div>
+                                    </a>
                                 <?php
                                 }
                                 ?>
@@ -117,16 +121,29 @@ include_once "./api/db.php";
                 <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
+                    <div class="cent" onclick="pp(1)"><img src="./img/up.jpg" alt=""></div>
+                    <?php
+                    $images = $Image->all(['sh' => 1]);
+                    foreach ($images as $idx => $image) {
+                    ?>
+                        <div id="ssaa<?= $idx ?>" class="im cent">
+                            <img src="./img/<?= $image['img'] ?>" style="width:150px;height:103px;border:3px solid orange;margin:2px">
+                        </div>
+                    <?php
+                    }
+
+                    ?>
+                    <div class="cent" onclick="pp(2)"><img src="./img/dn.jpg" alt=""></div>
                     <script>
-                        var nowpage = 0,
-                            num = 0;
+                        var nowpage = 1,
+                            num = <?= $Image->count(['sh' => 1]); ?>;
 
                         function pp(x) {
                             var s, t;
                             if (x == 1 && nowpage - 1 >= 0) {
                                 nowpage--;
                             }
-                            if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+                            if (x == 2 && nowpage != num - 3) {
                                 nowpage++;
                             }
                             $(".im").hide()
